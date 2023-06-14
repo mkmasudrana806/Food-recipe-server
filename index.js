9dc3c0cac893091d31acb0572079fc36899df150
mkmasudrana806 <mkmasudrana806@gmail.com>
2023-06-14 23:07:34 +0600

    vercel json file added, all done

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
+
+//recipes data
+const recipes = require("./data/recipe.json");
+
+//chef data
+const chefs = require("./data/chef.json");
+
 const cors = require("cors");
 
-app.use(cors());
+const corsConfig = {
+  origin: "*",
+  Credentials: true,
+  methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+};
+app.use(cors(corsConfig));
 
+//recipes route
 app.get("/", (req, res) => {
-  res.send("yes you can now create any api");
+  res.send(recipes);
+});
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
+
+app.get("/recipe/:id", (req, res) => {
+  const id = req.params.id;
+  const recipeId =
+    recipes?.recipes.find((recipe) => recipe.recipe_id == id) || {};
+  res.send(recipeId);
+});
+
+//chef route
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const chefId = chefs?.chefs.find((chef) => chef.id == id) || {};
+  res.send(chefId);
+});
+
+app.get("/recipes/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const chefAllRecipes =
+    recipes?.recipes.filter((recipe) => recipe.chef_id == id) || {};
+  console.log(chefAllRecipes, "inside server");
+  res.send(chefAllRecipes);
 });
 
 app.listen(port, () => {
-  console.log("api server is running");
+  console.log(`Example app listening on port ${port}`);
 });
